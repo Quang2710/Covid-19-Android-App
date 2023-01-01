53f94803dd0600a92e33de379a8e64e8f0125f28
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2023-01-01 23:08:48 +0700

    add firebase

--- app/src/main/java/com/example/covid_19/LoginActivity.java
+++ app/src/main/java/com/example/covid_19/LoginActivity.java
@@ -1,27 +1,76 @@
 package com.example.covid_19;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText txt_user,txt_password;
+    private Button btn_login;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        final Button btnLogin = findViewById(R.id.btn_login);
+        btn_login = findViewById(R.id.btn_login);
+        txt_user = findViewById(R.id.txt_user);
+        txt_password = findViewById(R.id.txt_password);
+        mAuth = FirebaseAuth.getInstance();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //open Register activity
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left); // Animation between activity
+                login();
             }
         });
+    }
+
+    private void login()
+    {
+        String user,pass;
+        user = txt_user.getText().toString();
+        pass = txt_password.getText().toString();
+
+        if(TextUtils.isEmpty(user))
+        {
+            Toast.makeText(this, "Username not empty", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(pass))
+        {
+            Toast.makeText(this, "Password not empty", Toast.LENGTH_SHORT).show();
+        }
+        //call and check login firebase
+        mAuth.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(), "Login successfull", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left); // Animation between activity
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Login faild!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
