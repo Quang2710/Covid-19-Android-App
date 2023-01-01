44a6d07aefcfad48e9f29a9dfd5640be651ddff1
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2023-01-02 02:49:03 +0700

    create StatsticsActivity

--- app/src/main/java/com/example/covid_19/MainActivity.java
+++ app/src/main/java/com/example/covid_19/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_statis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_statis = findViewById(R.id.btn_statis);
+
+        btn_statis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StatisticsActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation between activity
+            }
+        });
     }
 }
