2a3fb52644c25bf9aee38ef06bf7f047ab8d4d51
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2023-01-02 15:17:13 +0700

    binding data Statistics

--- app/src/main/java/com/example/covid_19/StatisticsActivity.java
+++ app/src/main/java/com/example/covid_19/StatisticsActivity.java
@@ -2,13 +2,32 @@ package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    TextView affected,dead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+        affected = findViewById(R.id.affected);
+        dead = findViewById((R.id.dead));
+        showData();
+
+    }
+    public void showData(){
+        Intent intent = getIntent();
+        String Affected = intent.getStringExtra("Affected");
+        String Dead = intent.getStringExtra("Dead");
+//        FirebaseDatabase.getInstance().getReference().child("Global").push().child("Affected");
+//        FirebaseDatabase.getInstance().getReference().child("Global").push().child("Dead");
+        affected.setText(Affected);
+        dead.setText(Dead);
     }
 }
