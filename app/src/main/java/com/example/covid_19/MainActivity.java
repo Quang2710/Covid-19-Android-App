9d16ff792eef2b59e23e4114c336765b94cde62b
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2023-01-03 23:28:40 +0700

    call action

--- app/src/main/java/com/example/covid_19/MainActivity.java
+++ app/src/main/java/com/example/covid_19/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.covid_19;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_statis;
+    private static final int REQUEST_CALL = 1;
+    Button btn_call,btn_sms,btn_statis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_statis = findViewById(R.id.btn_statis);
-
+        btn_call = findViewById(R.id.btn_call);
+        btn_sms = findViewById(R.id.btn_sms);
         btn_statis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +33,40 @@ public class MainActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation between activity
             }
         });
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call();
+            }
+        });
+    }
+    private void Call()
+    {
+        String number = "0857102032";
+        if(number.trim().length()>0)
+        {
+            if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},REQUEST_CALL);
+            }
+            else{
+                String dial = "tel:" + number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Call();
+            }
+            else
+            {
+                Toast.makeText(this, "Permisson denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
